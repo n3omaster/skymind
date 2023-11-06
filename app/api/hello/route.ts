8bafd1260c5c2f58ee9224e763feb6bff34d8848
Erich Garcia Cruz <erichjosegarcia@gmail.com>
2023-11-06 16:11:45 -0600

    Hero Section Almost Completed Missing links to modals and AI integrations

--- app/api/hello/route.ts
+++ app/api/hello/route.ts
@@ -1,3 +1,10 @@
+/**
+ * Api Endpoint for /api/hello
+ *
+ * @param request
+ * @returns
+ */
+
 export async function GET(request: Request) {
-  return new Response('Hello, Next.js!')
+  return new Response("Hello!");
 }

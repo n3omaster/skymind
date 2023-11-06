30fad57d8db75783fb548a2659afc657b8bea67a
Erich Garcia Cruz <erichjosegarcia@gmail.com>
2023-11-06 17:23:53 -0600

    Feature 1 Cmpleted

--- components/features.tsx
+++ components/features.tsx
@@ -21,16 +21,14 @@ export default function Features() {
                 {/* Content #1 */}
                 <div>
                   <div className="inline-flex font-medium bg-clip-text text-transparent bg-gradient-to-r from-purple-500 to-purple-200 pb-3">
-                    The security first platform
+                    With your idea in mind, we transform in reality
                   </div>
                 </div>
                 <h3 className="h3 bg-clip-text text-transparent bg-gradient-to-r from-slate-200/60 via-slate-200 to-slate-200/60 pb-3">
-                  Simplify your security with authentication services
+                  Simplify your business with automation tools & services
                 </h3>
                 <p className="text-lg text-slate-400 mb-8">
-                  Define access roles for the end-users, and extend your
-                  authorization capabilities to implement dynamic access
-                  control.
+                  Create your own software requirements and we will deliver it.
                 </p>
                 <div className="mt-8 max-w-xs max-md:mx-auto space-y-2">
                   <button
@@ -49,7 +47,7 @@ export default function Features() {
                     >
                       <path d="M14 0a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2h12Zm0 14V2H2v12h12Zm-3-7H5a1 1 0 1 1 0-2h6a1 1 0 0 1 0 2Zm0 4H5a1 1 0 0 1 0-2h6a1 1 0 0 1 0 2Z" />
                     </svg>
-                    <span>Simplify your security</span>
+                    <span>Improved Security</span>
                   </button>
                   <button
                     className={`flex items-center text-sm font-medium text-slate-50 rounded border bg-slate-800/25 w-full px-3 py-2 transition duration-150 ease-in-out hover:opacity-100 ${
@@ -67,7 +65,7 @@ export default function Features() {
                     >
                       <path d="M2 6H0V2a2 2 0 0 1 2-2h4v2H2v4ZM16 6h-2V2h-4V0h4a2 2 0 0 1 2 2v4ZM14 16h-4v-2h4v-4h2v4a2 2 0 0 1-2 2ZM6 16H2a2 2 0 0 1-2-2v-4h2v4h4v2Z" />
                     </svg>
-                    <span>Customer identity</span>
+                    <span>Top notch technology</span>
                   </button>
                   <button
                     className={`flex items-center text-sm font-medium text-slate-50 rounded border bg-slate-800/25 w-full px-3 py-2 transition duration-150 ease-in-out hover:opacity-100 ${
@@ -85,7 +83,7 @@ export default function Features() {
                     >
                       <path d="M14.3.3c.4-.4 1-.4 1.4 0 .4.4.4 1 0 1.4l-8 8c-.2.2-.4.3-.7.3-.3 0-.5-.1-.7-.3-.4-.4-.4-1 0-1.4l8-8ZM15 7c.6 0 1 .4 1 1 0 4.4-3.6 8-8 8s-8-3.6-8-8 3.6-8 8-8c.6 0 1 .4 1 1s-.4 1-1 1C4.7 2 2 4.7 2 8s2.7 6 6 6 6-2.7 6-6c0-.6.4-1 1-1Z" />
                     </svg>
-                    <span>Adaptable authentication</span>
+                    <span>BBB Certified</span>
                   </button>
                 </div>
               </div>
